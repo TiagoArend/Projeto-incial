78748dacc23c626a6fc78102c55d371fd84a9c0e
TiagoArend <tiaagoareend@hotmail.com>
2024-02-11 12:41:38 -0300

    alterando para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log (numeroSecreto)
 let tentativas = 1;
@@ -10,7 +10,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 function ExibirMensagem() {
     exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 50');
 }
 ExibirMensagem()
 
